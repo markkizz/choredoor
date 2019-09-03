46b7c6e4b0b6b507d70ed7004a9a9035ecf3fda5
markkizz <mark.pattanapara@gmail.com>
2019-09-03 20:51:11 +0700

    finish style a web game

---
+++ script.js
@@ -0,0 +1,18 @@
+//declare the object
+var doorImage1 = document.getElementById("door1");
+var doorImage2 = document.getElementById("door2");
+var doorImage3 = document.getElementById("door3");
+var botDoor = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg';
+var beachDoor = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
+var spaceDoor = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
+
+//door event
+doorImage1.onclick = () =>{
+    doorImage1.src = botDoor;
+}
+doorImage2.onclick = () =>{
+    doorImage2.src = beachDoor;
+}
+doorImage3.onclick = () =>{
+    doorImage3.src = spaceDoor;
+}
