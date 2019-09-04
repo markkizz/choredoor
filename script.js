48e92126aa6f6ffc52d0ae696796b4cdc609009d
markkizz <mark.pattanapara@gmail.com>
2019-09-04 22:46:22 +0700

    finish bug fix door cannot open when we found bot

--- script.js
+++ script.js
@@ -5,28 +5,27 @@ var doorImage3 = document.getElementById("door3");
 var botDoor = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg';
 var beachDoor = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
 var spaceDoor = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
+var closeDoor = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg';
 var btStart = document.getElementById("start");
 var doorAll = document.querySelectorAll(".door-frame");
-var foundBot = true;
+var foundBot = false;
 //Door random
 var numClosedDoors = 3;
 var openDoor1;
 var openDoor2;
 var openDoor3;
 
-const randomChoreDoorGenerator = () =>{
+const randomChoreDoorGenerator = () => {
     var choreDoor = Math.floor(Math.random() * numClosedDoors);
-    if(choreDoor === 0){
+    if (choreDoor === 0) {
         openDoor1 = botDoor;
         openDoor2 = beachDoor;
         openDoor3 = spaceDoor;
-    }
-    else if(choreDoor ===1){
+    } else if (choreDoor === 1) {
         openDoor2 = botDoor;
         openDoor1 = beachDoor;
         openDoor3 = spaceDoor;
-    }
-    else {
+    } else {
         openDoor3 = botDoor;
         openDoor2 = beachDoor;
         openDoor1 = spaceDoor;
@@ -35,55 +34,70 @@ const randomChoreDoorGenerator = () =>{
 //Winner diplay
 var doorImgChange = [botDoor, beachDoor, spaceDoor];
 
-const playDoor = (doorImg) =>{
+var playDoor = (doorImg) => {
     //Decrease numClosedDoors
-    for(let i=0 ; i<doorImgChange.length ; i++){
-        if(doorImg === doorImgChange[i]){
+    for (let i = 0; i < doorImgChange.length; i++) {
+        if (doorImg === doorImgChange[i]) {
             numClosedDoors -= 1;
-            console.log(numClosedDoors);//d
-            console.log(!foundBot);//d
+            console.log(numClosedDoors); //d
+            console.log(!foundBot); //d
         }
     }
     //winner
-    if(numClosedDoors === 0 && doorImg === botDoor){
+    if (numClosedDoors === 0 && doorImg === botDoor) {
         //yeah! you a winner
-        btStart.textContent = "You win! Play again?"
+        btStart.textContent = "You win! Play again?";
     }
     //game over when found Bot
-    if(numClosedDoors !== 0 && doorImg === botDoor){
+    if (numClosedDoors !== 0 && doorImg === botDoor) {
         //sorry game over
-        btStart.textContent = "Game over! Play again?"
+        btStart.textContent = "Game over! Play again?";
+        foundBot = true;
     }
 }
 
-//reset game
-const gameReset = () =>{
-    btStart.onclick = () =>{
-        for(let i=0 ; i<doorAll.length ; i++){
-            doorAll[i].src = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg";
-        }
-        numClosedDoors = 3;
-        btStart.textContent = "Good luck!";
-    }
+//check bot
+// var isBot = (doorimg) =>{
 
-    // score collect
-}
-//TODO: fixed the bug when open the door and bot is found but other door still working
+// }
+
 //door event
-if(foundBot){
-    doorImage1.onclick = () =>{
+doorImage1.onclick = () => {
+    if(!foundBot){
         doorImage1.src = openDoor1;
         playDoor(doorImage1.src);
-        foundBot = false;
     }
-    doorImage2.onclick = () =>{
+
+}
+doorImage2.onclick = () => {
+    if(!foundBot){
         doorImage2.src = openDoor2;
         playDoor(doorImage2.src);
     }
-    doorImage3.onclick = () =>{
+    
+}
+doorImage3.onclick = () => {
+    if(!foundBot){
         doorImage3.src = openDoor3;
         playDoor(doorImage3.src);
     }
+    
+}
+
+
+//reset game
+const gameReset = () => {
+    btStart.onclick = () => {
+        for (let i = 0; i < doorAll.length; i++) {
+            doorAll[i].src = closeDoor;
+        }
+        numClosedDoors = 3;
+        btStart.textContent = "Good luck!";
+        foundBot = false;
+        randomChoreDoorGenerator();
+    }
+
+    // score collect
 }
 
 //excute function
