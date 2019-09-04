25621c950b7a6ac5cca298d67aa047df42129f61
markkizz <mark.pattanapara@gmail.com>
2019-09-04 13:23:25 +0700

    finish Door random go to _Let's Make This ACT Like a Game! - Part II_

--- script.js
+++ script.js
@@ -5,15 +5,43 @@ var doorImage3 = document.getElementById("door3");
 var botDoor = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg';
 var beachDoor = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
 var spaceDoor = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
+//Door random
+var numClosedDoors = 3;
+var openDoor1;
+var openDoor2;
+var openDoor3;
+
+
+const randomChoreDoorGenerator = () =>{
+    var choreDoor = Math.floor(Math.random() * numClosedDoors);
+    if(choreDoor === 0){
+        openDoor1 = botDoor;
+        openDoor2 = beachDoor;
+        openDoor3 = spaceDoor;
+    }
+    else if(choreDoor ===1){
+        openDoor2 = botDoor;
+        openDoor1 = beachDoor;
+        openDoor3 = spaceDoor;
+    }
+    else {
+        openDoor3 = botDoor;
+        openDoor2 = beachDoor;
+        openDoor1 = spaceDoor;
+    }
+}
 
 //door event
 doorImage1.onclick = () =>{
-    doorImage1.src = botDoor;
+    doorImage1.src = openDoor1;
 }
 doorImage2.onclick = () =>{
-    doorImage2.src = beachDoor;
+    doorImage2.src = openDoor2;
 }
 doorImage3.onclick = () =>{
-    doorImage3.src = spaceDoor;
+    doorImage3.src = openDoor3;
 }
-// TODO: create through a task"Let's Make This ACT Like a Game! - Part I"
+
+
+//excute function
+randomChoreDoorGenerator();
