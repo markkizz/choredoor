521f6d13beaf1a4f769efccae2ecae7157b5051c
markkizz <mark.pattanapara@gmail.com>
2019-09-10 20:37:05 +0700

    add to new repo

---
+++ test.js
@@ -0,0 +1,29 @@
+// var door = ['a', 'b', 'c'];
+// var newDoor;
+// var numRandom = [-1, -1 ,-1];
+// function random(num) {
+//     return Math.floor(Math.random() * num);
+// } 
+// var numDoorRandom = random(door.length)
+// function doorRandom(num){
+//     numRandom[0] = numDoorRandom;
+//     for(let i=1 ; i<door.length ; i++){
+//         let loop = true;
+//         while(loop){
+//             numRandom[i] = numDoorRandom;
+//             if(numRandom[i] !== numRandom[i-1]){
+//                 loop = false;
+//             }
+            
+//         }
+
+        
+
+//     }
+//     //newDoor = [door[numRandom[0]], ];
+    
+// }
+// doorRandom(3);
+console.log('hello');
+
+
